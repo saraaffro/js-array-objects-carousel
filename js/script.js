7e945bdd04d4538ce43190056bab04780973ca88
saraaffro <saraaffro@gmail.com>
2023-12-04 13:52:13 +0100

    feat: add title and description style: change img width

--- js/script.js
+++ js/script.js
@@ -31,18 +31,28 @@ const carousel =
     <div class="element-container">
         <div class="element active">
             <img src="${images[0].image}" alt="immagine">
+            <div class="title">${images[0].title}</div>
+            <div class="description">${images[0].text}</div>
         </div>
         <div class="element">
             <img src="${images[1].image}" alt="immagine">
+            <div class="title">${images[1].title}</div>
+            <div class="description">${images[1].text}</div>
         </div>
         <div class="element">
             <img src="${images[2].image}" alt="immagine">
+            <div class="title">${images[2].title}</div>
+            <div class="description">${images[2].text}</div>
         </div>
         <div class="element">
             <img src="${images[3].image}" alt="immagine">
+            <div class="title">${images[3].title}</div>
+            <div class="description">${images[3].text}</div>
         </div>
         <div class="element">
             <img src="${images[4].image}" alt="immagine">
+            <div class="title">${images[4].title}</div>
+            <div class="description">${images[4].text}</div>
         </div>
         <div class="next-button"><i class="fa-solid fa-arrow-up"></i></div>
         <div class="back-button"><i class="fa-solid fa-arrow-down"></i></div>
